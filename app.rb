c8310d1584b745ae8eb475ce99826f49e0bc852e
Emily Watkins and Nicko Dela Cruz <emily@>
2018-03-01 10:53:31 -0800

    add functional code to app.rb

--- app.rb
+++ app.rb
@@ -2,3 +2,17 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+
+get('/') do
+  @recipes = Recipe.all
+  erb(:index)
+end
+
+post('/') do
+  name = params['name']
+  @recipes = Recipe.all
+  recipe = Recipe.create({:name => name, :instruction => nil})
+  erb(:index)
+end
