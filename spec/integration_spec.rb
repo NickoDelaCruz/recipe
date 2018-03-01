2b3c9567be93474eb0d99df816df7bbb1a6cc89b
Emily Watkins and Nicko Dela Cruz <nicko@>
2018-03-01 11:02:56 -0800

    add integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,1 +1,10 @@
 require("spec_helper")
+
+describe 'the recipe creation path', {:type => :feature} do
+  it 'takes the user to the homepage where they can create a project' do
+    visit '/'
+    fill_in('name', :with => 'Casserole')
+    click_button('Add recipe')
+    expect(page).to have_content('Casserole')
+  end
+end
